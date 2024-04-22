74be733f7de059a7c53fd93aa9f86c5dce7dc09c
richie007dev <richie@ardhika.com>
2024-04-22 23:36:05 +0530

    Added dummy data.

---
+++ lib/data/groceryList.dart
@@ -0,0 +1,74 @@
+class GroceryList {
+ static List<Map<String, dynamic>> groceryList = [
+    {
+      'id': 1,
+      'name': 'Apples',
+      'description': 'Fresh red apples',
+      'price': 1.5,
+      'imageUrl': 'https://images.everydayhealth.com/images/diet-nutrition/apples-101-about-1440x810.jpg?sfvrsn=f86f2644_1',
+    },
+    {
+      'id': 2,
+      'name': 'Milk',
+      'description': 'Whole milk',
+      'price': 3.0,
+      'imageUrl': 'https://www.allrecipes.com/thmb/TgXszUn8XHUTe19kOwlyGQ4ZjyM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-87984584-2-2000-b40af34c16fd4bed815f1e279592b172.jpg',
+    },
+    {
+      'id': 3,
+      'name': 'Bread',
+      'description': 'Whole wheat bread',
+      'price': 2.0,
+      'imageUrl': 'https://www.allrecipes.com/thmb/CjzJwg2pACUzGODdxJL1BJDRx9Y=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/6788-amish-white-bread-DDMFS-4x3-6faa1e552bdb4f6eabdd7791e59b3c84.jpg',
+    },
+    {
+      'id': 4,
+      'name': 'Chicken Breast',
+      'description': 'Boneless skinless chicken breast',
+      'price': 5.0,
+      'imageUrl': 'https://www.greenchickchop.in/cdn/shop/files/ChickenBreastBoneless.webp?v=1682572347',
+    },
+    {
+      'id': 5,
+      'name': 'Bananas',
+      'description': 'Ripe bananas',
+      'price': 2.0,
+      'imageUrl': 'https://www.bobtailfruit.co.uk/media/mageplaza/blog/post/4/2/42e9as7nataai4a6jcufwg.jpeg',
+    },
+    {
+      'id': 6,
+      'name': 'Eggs',
+      'description': 'Large eggs',
+      'price': 2.5,
+      'imageUrl': 'https://cdn.britannica.com/94/151894-050-F72A5317/Brown-eggs.jpg',
+    },
+    {
+      'id': 7,
+      'name': 'Cheese',
+      'description': 'Cheddar cheese',
+      'price': 4.0,
+      'imageUrl': 'https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/healthiest-cheese-1296x728-swiss.jpg?w=1155&h=1528',
+    },
+    {
+      'id': 8,
+      'name': 'Tomatoes',
+      'description': 'Fresh tomatoes',
+      'price': 3.0,
+      'imageUrl': 'https://blog.lexmed.com/images/librariesprovider80/blog-post-featured-images/shutterstock_1896755260.jpg?sfvrsn=52546e0a_0',
+    },
+    {
+      'id': 9,
+      'name': 'Pasta',
+      'description': 'Whole wheat pasta',
+      'price': 2.5,
+      'imageUrl': 'https://hips.hearstapps.com/hmg-prod/images/marry-me-pasta-index-65b7c910a7bbf.jpg',
+    },
+    {
+      'id': 10,
+      'name': 'Salmon Fillet',
+      'description': 'Fresh salmon fillet',
+      'price': 7.0,
+      'imageUrl': 'https://vikalinka.com/wp-content/uploads/2022/06/Air-Fryer-Salmon-3-Edit-500x500.jpg',
+    },
+  ];
+}
