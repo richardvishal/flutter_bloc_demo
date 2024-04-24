e8c77f1509b32896f4ee82930a0619a2a90646e9
richie007dev <richie@ardhika.com>
2024-04-24 12:44:49 +0530

    Wrap scaffold inside BlocConsumer

--- lib/features/home/ui/home.dart
+++ lib/features/home/ui/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_demo/features/home/bloc/home_bloc.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,17 +10,41 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final HomeBloc homeBloc = HomeBloc();
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        title: const Text('Richie Grocery App', style: TextStyle(color: Colors.white),),
-        backgroundColor: Colors.blueAccent,
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text('Home'),
-      ),
+    return BlocConsumer<HomeBloc, HomeState>(
+      bloc: homeBloc,
+      // listenWhen: (previous, current) {},
+      // buildWhen: (previous, current) {},
+      listener: (BuildContext context, Object? state) {},
+      builder: (BuildContext context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              'Richie Grocery App',
+              style: TextStyle(color: Colors.white),
+            ),
+            backgroundColor: Colors.blueAccent,
+            centerTitle: true,
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.favorite_border),
+                color: Colors.white,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.shopping_bag_outlined),
+                color: Colors.white,
+              ),
+            ],
+          ),
+          body: const Center(
+            child: Text('Home'),
+          ),
+        );
+      },
     );
   }
 }
