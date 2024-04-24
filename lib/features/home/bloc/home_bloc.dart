eb10095f1bc2f471867313dbdc1010b21edb5a75
richie007dev <richie@ardhika.com>
2024-04-24 15:39:50 +0530

    Added event listeners in home_bloc.dart

--- lib/features/home/bloc/home_bloc.dart
+++ lib/features/home/bloc/home_bloc.dart
@@ -1,13 +1,46 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
 
 part 'home_event.dart';
 part 'home_state.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   HomeBloc() : super(HomeInitial()) {
-    on<HomeEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+    on<HomeProductWishlistButtonClickedEvent>(
+      homeProductWishlistButtonClickedEvent,
+    );
+    on<HomeProductCartButtonClickedEvent>(
+      homeProductCartButtonClickedEvent,
+    );
+    on<HomeWishlistButtonNavigateEvent>(
+      homeWishlistButtonNavigateEvent,
+    );
+
+    on<HomeCartButtonNavigateEvent>(
+      homeCartButtonNavigateEvent,
+    );
+  }
+
+  FutureOr<void> homeProductWishlistButtonClickedEvent(
+      HomeProductWishlistButtonClickedEvent event, Emitter<HomeState> emit) {
+    print('Wishlist Button Clicked');
+  }
+
+  FutureOr<void> homeProductCartButtonClickedEvent(
+      HomeProductCartButtonClickedEvent event, Emitter<HomeState> emit) {
+    print('Cart Button Clicked');
+  }
+
+  FutureOr<void> homeWishlistButtonNavigateEvent(
+      HomeWishlistButtonNavigateEvent event, Emitter<HomeState> emit) {
+    print('Wishlist Navigate');
+    emit(HomeNavigateToCartActionState());
+  }
+
+  FutureOr<void> homeCartButtonNavigateEvent(
+      HomeCartButtonNavigateEvent event, Emitter<HomeState> emit) {
+    print('Cart Navigate');
+    emit(HomeNavigateToCartActionState());
   }
 }
