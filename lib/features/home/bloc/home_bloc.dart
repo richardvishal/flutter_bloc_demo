57efb8d7efd41a85c015c6f0cb82409812b7c86a
richie007dev <richie@ardhika.com>
2024-04-24 18:02:53 +0530

    Added new event and state.

--- lib/features/home/bloc/home_bloc.dart
+++ lib/features/home/bloc/home_bloc.dart
@@ -1,12 +1,17 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc_demo/data/groceryList.dart';
+import 'package:flutter_bloc_demo/features/home/models/product_data_model.dart';
 
 part 'home_event.dart';
 part 'home_state.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   HomeBloc() : super(HomeInitial()) {
+    on<HomeInitialEvent>(
+      homeInitialEvent,
+    );
     on<HomeProductWishlistButtonClickedEvent>(
       homeProductWishlistButtonClickedEvent,
     );
@@ -19,6 +24,28 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
 
     on<HomeCartButtonNavigateEvent>(
       homeCartButtonNavigateEvent,
+    );
+  }
+  FutureOr<void> homeInitialEvent(
+      HomeInitialEvent event, Emitter<HomeState> emit) async {
+    emit(HomeLoadingState());
+    await Future.delayed(
+      const Duration(seconds: 3),
+    );
+
+    emit(
+      HomeLoadedSuccessState(
+        products: GroceryList.groceryProducts
+            .map(
+              (e) => ProductDataModel(
+                  id: e['id'],
+                  name: e['name'],
+                  description: e['description'],
+                  price: e['price'],
+                  imageUrl: e['imageUrl']),
+            )
+            .toList(),
+      ),
     );
   }
 
